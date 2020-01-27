df0e831195959725e139b842468ff0ad8fde44d0
SrinivasaChennaKrishnan <53043139+SrinivasaChennaKrishnan@users.noreply.github.com>
2020-01-27 12:10:26 +0530

    Test case modifications
    Test case modifications

--- src/__tests__/GenPinFunction.test.js
+++ src/__tests__/GenPinFunction.test.js
@@ -1,55 +1,55 @@
-import { GenPinFunction } from '../Common/GenPinFunction';
-import { checkDupNum } from '../Common/GenPinFunction';
-import { checkDescendNumbers } from '../Common/GenPinFunction';
-import { checkAscendNumbers } from '../Common/GenPinFunction';
-import { checkUniqPins } from '../Common/GenPinFunction';
-import EnzymeAdapter from 'enzyme-adapter-react-16';
-import { configure } from 'enzyme';
+import { GenPinFunction } from "../Common/GenPinFunction";
+import { checkDupNum } from "../Common/GenPinFunction";
+import { checkDescendNumbers } from "../Common/GenPinFunction";
+import { checkAscendNumbers } from "../Common/GenPinFunction";
+import { checkUniqPins } from "../Common/GenPinFunction";
+import EnzymeAdapter from "enzyme-adapter-react-16";
+import { configure } from "enzyme";
 
 configure({ adapter: new EnzymeAdapter() });
-describe('Generate Function', () => {
-    test('Testing GenPinFunction', () => {
-        let inst = GenPinFunction();
-        expect(inst).not.toBe(null);
-    })
-    test('Testing GenPinFunction', () => {
-        let inst = GenPinFunction();
-        expect(typeof inst).toBe("object");
-    })
-    test('Test DUPLICATE Number Function', () => {
-        let inst = checkDupNum(10);
-        expect(inst).toBe(false);
-    })
-    test('Test DESCENDING Function with the string ', () => {
-        let inst = checkDescendNumbers("testing");
-        expect(inst).toBe("gnitset");
-    })
-    test('Test SEQUENCE Function', () => {
-        let inst = checkAscendNumbers(10);
-        expect(inst).toBe(false);
-    })
-    test('Test DUPLICATE Function', () => {
-        let inst = checkDupNum(10);
-        expect(inst).toBe(false);
-    })
-    test('Test DESCENDING function', () => {
-        let inst = checkDescendNumbers("reverse");
-        expect(inst).toBe("esrever");
-    })
-    test('Test SEQUENCE Function', () => {
-        let inst = checkAscendNumbers(10);
-        expect(inst).toBe(false);
-    })
-    test('Test UNIQUE pins Function', () => {
-        let inst = checkUniqPins(['4785','8564','1542']);
-        expect(inst).toBe(inst);
-    })
-    test('Test UNIQUE pins Function', () => {
-        let inst = checkUniqPins(['4785','8564','1542']);
-        expect(inst).not.toBe([]);
-    })
-    test('Test UNIQUE pins Function', () => {
-        let inst = checkUniqPins(['4785','8564','1542']);
-        expect(typeof inst).toBe('object');
-    })
-})
+describe("Generate pin Function", () => {
+  test("Test GenPinFunction", () => {
+    let inst = GenPinFunction();
+    expect(inst).not.toBe(null);
+  });
+  test("Test GenPinFunction output type", () => {
+    let inst = GenPinFunction();
+    expect(typeof inst).toBe("object");
+  });
+  test("Test DUPLICATE Number Function", () => {
+    let inst = checkDupNum(10);
+    expect(inst).toBe(false);
+  });
+  test("Test DESCENDING Function with the string ", () => {
+    let inst = checkDescendNumbers("testing");
+    expect(inst).toBe("gnitset");
+  });
+  test("Test SEQUENCE Function", () => {
+    let inst = checkAscendNumbers(10);
+    expect(inst).toBe(false);
+  });
+  test("Test DUPLICATE Function", () => {
+    let inst = checkDupNum(10);
+    expect(inst).toBe(false);
+  });
+  test("Test DESCENDING function", () => {
+    let inst = checkDescendNumbers("reverse");
+    expect(inst).toBe("esrever");
+  });
+  test("Test SEQUENCE Function", () => {
+    let inst = checkAscendNumbers(10);
+    expect(inst).toBe(false);
+  });
+  test("Test UNIQUE pins Function", () => {
+    let inst = checkUniqPins(["4785", "8564", "1542"]);
+    expect(inst).toBe(inst);
+  });
+  test("Test UNIQUE pins Function", () => {
+    let inst = checkUniqPins(["4785", "8564", "1542"]);
+    expect(inst).not.toBe([]);
+  });
+  test("Test UNIQUE pins Function", () => {
+    let inst = checkUniqPins(["4785", "8564", "1542"]);
+    expect(typeof inst).toBe("object");
+  });
+});
--- src/__tests__/GeneratePinTab.test.js
+++ src/__tests__/GeneratePinTab.test.js
@@ -1,31 +1,32 @@
-import React from 'react';
-import { shallow, mount } from 'enzyme';
-import { GeneratePinTab } from '../Components/GeneratePinTab';
-import EnzymeAdapter from 'enzyme-adapter-react-16';
-import { configure } from 'enzyme';
+import React from "react";
+import { shallow } from "enzyme";
+import { GeneratePinTab } from "../Components/GeneratePinTab";
+import EnzymeAdapter from "enzyme-adapter-react-16";
+import { configure } from "enzyme";
 configure({ adapter: new EnzymeAdapter() });
 let PropVal = {
-    userObject: [], generatedPin: []
+  userObject: [],
+  generatedPin: []
 };
-describe('Generate Tab Test Cases', () => {
-    let wrapper;
-    wrapper = shallow(<GeneratePinTab {...PropVal} />)
-    test('Testing Snapshot', () => {
-        expect(wrapper).toMatchSnapshot();
-    })
-    test('Testing Length', () => {
-        expect(wrapper.length).not.toBe(null);
-    })    
-    test('Testing Generate Button Text', () => {
-        expect(wrapper.find('#generate').text()).toEqual('GENERATE');
-    })
-    test('Testing Save Button Text', () => {
-        expect(wrapper.find('#save').text()).toEqual('SAVE');
-    })
-    test('Testing Divs Length', () => {
-        expect(wrapper.find('div').length).toEqual(1);
-    })
-    test('Testing Buttons Length', () => {
-        expect(wrapper.find('button').length).toEqual(2);
-    })
-})
+describe("Generate pin Tab Test Cases", () => {
+  let wrapper;
+  wrapper = shallow(<GeneratePinTab {...PropVal} />);
+  test("Test Snapshot", () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+  test("Test Length", () => {
+    expect(wrapper.length).not.toBe(null);
+  });
+  test("Test Generate Button Text", () => {
+    expect(wrapper.find("#generate").text()).toEqual("GENERATE");
+  });
+  test("Test Save Button Text", () => {
+    expect(wrapper.find("#save").text()).toEqual("SAVE");
+  });
+  test("Test Div count", () => {
+    expect(wrapper.find("div").length).toEqual(1);
+  });
+  test("Test Button count", () => {
+    expect(wrapper.find("button").length).toEqual(2);
+  });
+});
